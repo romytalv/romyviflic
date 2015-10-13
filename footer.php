9343099098cc3ed2c04b86a1f6b1b119db9ac2fd
romytalv <romyviflic@gmail.com>
2015-10-13 20:14:59 +0100

    Fix indentation

--- footer.php
+++ footer.php
@@ -5,12 +5,12 @@
         			<a href="mailto:romyviflic@gmail.com"><i class="fa fa-envelope-o"></i></a>
         		</div>
 
-				<!-- copyright -->
-				<p class="copyright">
-					&copy; <?php echo date('Y'); ?> Copyright <?php bloginfo('name'); ?>. <?php _e('Powered by', 'html5blank'); ?>
-					<a href="//wordpress.org" title="WordPress">WordPress</a> &amp; <a href="//html5blank.com" title="HTML5 Blank">HTML5 Blank</a>.
-				</p>
-				<!-- /copyright -->
+			<!-- copyright -->
+			<p class="copyright">
+				&copy; <?php echo date('Y'); ?> Copyright <?php bloginfo('name'); ?>. <?php _e('Powered by', 'html5blank'); ?>
+				<a href="//wordpress.org" title="WordPress">WordPress</a> &amp; <a href="//html5blank.com" title="HTML5 Blank">HTML5 Blank</a>.
+			</p>
+			<!-- /copyright -->
 
 			</footer>
 			<!-- /footer -->
